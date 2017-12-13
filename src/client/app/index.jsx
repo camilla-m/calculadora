7dc7c52f3b1a6552752160cc235f21049dabd81a
Victória Aiello <vaiello@rlicorp.com>
2017-12-13 11:11:19 -0200

    react working

---
+++ src/client/app/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return (
+	    <div class="container text-xs-center">
+	      <div class="row">
+	        <div class="col-sm-6 offset-sm-3">
+	          <p>Quero hospedar 1 site ↓ para uso pessoal ↓<br/>
+	            Programado(s) em PHP ↓ / Utilizando plataforma WordPress ↓<br/>
+	            Em ambiente compartilhado ↓ (?)<br/>
+	            Com 1 banco(s) de dados MySQL ↓<br/>
+	            Espero receber até 1000 visitas por mês ↓ em cada um ↓ (d)eles</p>
+
+	          <p><span class="h4">A hospedagem do seu <em>site</em> custará:</span><br/>
+	          <span class="h2">R$6,00  por mês no plano Site Pro da Umbler</span><br/>
+	          ou em média R$25,00 por mês em outras empresas </p>
+
+
+	          <p><a href="#">Receba este resultado por email</a></p>
+
+	          <p>Ganhe até R$100 em créditos para hospedar seu site WordPress na Umbler. 
+	          Comece agora: <input type="text" name="email" /> <button>TESTAR GRÁTIS</button></p>
+
+	          <p>Ainda com dúvidas? Fale com um especialista em hospedagem de sites <em>WordPress</em>.</p>
+
+	          <p>Texto legal afirmando que trata-se de uma simulação e dando detalhes sobre os recursos específicos de cada plano, banco de dados, etc.</p>
+	        </div>
+	      </div>
+	    </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [
+      path.resolve('./js'),
+      path.resolve('./node_modules')
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader'])
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+              presets: ['es2015']
+          }
+      }
+    ]
+  },
+};
+
+module.exports = config;
